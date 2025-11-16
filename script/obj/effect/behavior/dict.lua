bf3ba4669540217a665d6d91cc78b2c26ba9ec48
muyu6026 <mo930396061@163.com>
2025-11-16 22:26:01 +0800

    Create dict.lua

---
+++ script/obj/effect/behavior/dict.lua
@@ -0,0 +1,6 @@
+return function (datas)
+    local dict = {
+        ['$$default_units_ts.behavior.默认骑乘交互.root'] = datas.behavior['$$default_units_ts.behavior.默认骑乘交互']['root'],
+    }
+    return dict
+end
